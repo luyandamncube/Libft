5f547f8b0c8c63526a261e60649c3b00cd9d3a80
Luyanda MNCUBE <lmncube@e4r21p1.wethinkcode.co.za>
2018-05-24 16:25:39 +0200

    made changes

--- srcs/main.c
+++ srcs/main.c
@@ -1,14 +1,17 @@
-#include "libft.h"
 /*
-    USAGE
-    -gcc -Werror -Wextra -Wall main.c ./ft_funcname.c ../includes
-    -change lib_name in typedef line to libft function
-    -change sys_name in typedef line to system function
+    Usage: 
+    1   sh test.sh
+    2   ./test ft_name.c
 */
+#include "libft.h"
 
 int num[9] = {1,0,2147483647, -2147483648, 1024, 42, -42, 9999};
 char chr[12] = {'a','A','z','Z','\0',' ','\v','\n','\r','\t','\b','%'};
-char *str[10] = {"abcdefghijklmnopqrstuvwxyz","b\0ab","aa\n","01-156-12302147483647-21474838","***sup****!**", "abc\0\0\0","aaa\0aaa", "\0\0\0\0\0\0","\t\t\n\t\t   ","\t\n  \tAAA \t BBB\t\n  \t"};
+char *str[10] = {
+                    "abcdefghijklmnopqrstuvwxyz","b\0ab","aa\n",
+                    "01-156-12302147483647-21474838","***sup****!**", "abc\0\0\0",
+                    "aaa\0aaa", "\0\0\0\0\0\0","\t\t\n\t\t   ","\t\n  \tAAA \t BBB\t\n  \t"
+                };
 
 /* strlcat(strdup("abc\0\0\0"), "ccc", 0) == 3
     strlcat(strdup("abc\0\0\0"), "ccc", 1) == 4
@@ -24,30 +27,51 @@ char *str[10] = {"abcdefghijklmnopqrstuvwxyz","b\0ab","aa\n","01-156-12302147483
 # define ASSERT_NOT_EQUAL(X,Y)(X != Y ? TRUE : FALSE)          //Check actual != expected    
 # define ASSERT_PTR_NULL(X)(X == NOTHING ? TRUE : FALSE)       //Check pointer == NULL             
 # define ASSERT_PTR_NOT_NULL(X)(X != NOTHING ? TRUE : FALSE)   //Check pointer != NULL    
-
+void lastCharDel (char *name)
+{
+     int length;
+     length = strlen(name);//Get length of string
+     name[length - 2] = '\0';//Set next to last char to null
+     return;
+}
 //void t_1(s,int (*sys)(char*), int (*mine)(char *))
-int main()
+int main(int argc, char **argv)
 {   
     int i = 0;
     int pass_counter = 0;
+    char *ft_name = (argv[1]);
+    char ft_func = (char*)malloc(sizeof(char)*strlen(ft_name));
+    int func_len = strlen(ft_name);
+    ft_func[func_len-2] ='\0';
 
-    //Change fail count to match no of tests
-    while (i < 10)
+    printf("TESTS FOR %s:\n", ft_name);
+
+    if (argc == 2)
     {
-        if (strlen(str[i]) == ft_strlen(str[i]))
+        //Change fail count to match no of tests
+        while (i < 10)
         {
-            printf("PASS:  test %d\n", i);
-            pass_counter++;
+            if (strlen(str[i]) == ft_strlen(str[i]))
+            {
+                printf("PASS:  test %d\n", i+1);
+                pass_counter++;
+            }
+                
+            else
+            {
+                printf("FAIL:  test %d\n", i);
+            }
+                
+            i++;
         }
-            
+        if (pass_counter == i)
+            printf("SUCCESS!");
         else
-        {
-            printf("FAIL:  test %d\n", i);
-        }
-            
-        i++;
+            printf("Only %d functions passed...", pass_counter);
+        return (0);
     }
-    if (pass_counter == i)
-           printf("SUCCESS!");
-    return (0);
+    else 
+    {
+        printf("ERROR. Usage: './test ft_name.c'");
+    }
 }
---
+++ srcs/test.sh
@@ -0,0 +1,1 @@
+gcc -Wall -Werror -Wextra ft_strlen.c -I ../includes main.c -o test
