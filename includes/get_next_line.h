a9a8fd953a59f28033b3a4bc2f708bc9021e461c
Luyanda MNCUBE <lmncube@e4r21p1.wethinkcode.co.za>
2018-08-20 16:28:32 +0200

    fixed overrun issue

--- includes/get_next_line.h
+++ includes/get_next_line.h
@@ -6,13 +6,13 @@
 /*   By: lmncube <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/02 10:38:19 by lmncube           #+#    #+#             */
-/*   Updated: 2018/07/10 09:51:55 by lmncube          ###   ########.fr       */
+/*   Updated: 2018/08/20 16:27:35 by lmncube          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 8u
+# define BUFF_SIZE 1
 # include "libft.h"
 
 int		get_next_line(const int fd, char **line);
